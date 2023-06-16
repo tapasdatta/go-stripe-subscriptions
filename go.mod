25be6036e9cc89284624b8f03049c417fd1d79db
Tapas Datta <tapash.me@gmail.com>
2023-06-16 20:16:17 +0600

    created db connection

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/tapasdatta/go-stripe-subscriptions
 
 go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	gorm.io/driver/mysql v1.5.1
+	gorm.io/gorm v1.25.1
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
---
+++ models/setup.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to database")
+	}
+	DB = database
+}
